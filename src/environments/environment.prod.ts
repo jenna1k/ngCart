cfd8b82b01437c63de23a3d3ad9dc301bc48f3ad
jenna <jenna_kim1@hotmail.com>
2019-06-12 11:11:36 +0200

    firebase prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCobhd89NI24QpBxc5uj0WEsheIRWn3-N8",
+    authDomain: "ng-cart-tutorial.firebaseapp.com",
+    databaseURL: "https://ng-cart-tutorial.firebaseio.com",
+    projectId: "ng-cart-tutorial",
+    storageBucket: "ng-cart-tutorial.appspot.com",
+    messagingSenderId: "356935057924",
+    appId: "1:356935057924:web:025f34a85f8be137"
+  }
 };
 
 
